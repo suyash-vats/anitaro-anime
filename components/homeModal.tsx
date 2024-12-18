d5c48846e035e63d7ddc5d3b9969084f37bd419b
akshitvigg <akshitvig48@gmail.com>
2024-12-18 15:39:01 +0530

    div aligned

--- components/homeModal.tsx
+++ components/homeModal.tsx
@@ -61,31 +61,35 @@ export default function HomepageModal() {
   }, []);
 
   return (
-    <div className="pt-28  relative">
-      <div className="flex justify-center ">
-        <Link href={images[currImageIndex].link} passHref>
-          <div className="relative sm:w-[1350px] w-[340px]  h-[470px]">
-            <Image
-              src={images[currImageIndex].src}
-              alt={images[currImageIndex].alt}
-              fill
-              className="rounded-xl sm:object-cover sm:object-center"
-            />
-            <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black/80 via-black/30 to-transparent pointer-events-none"></div>
-          </div>
-        </Link>
-      </div>
-      <div className=" translate-x-14  sm:translate-x-28 -translate-y-32">
-        <p className=" ">{images[currImageIndex].year}</p>
-        <p className=" font-bold text-md sm:text-4xl">
-          {images[currImageIndex].name}
-        </p>
-        <div>
-          <Link href={images[currImageIndex].link}>
-            <button className=" hover:bg-gray-300 active:scale-105 transition-all duration-300 font-semibold px-3 sm:px-4 mt-3 rounded-md py-2 bg-white text-black">
+    <div className="pt-28 relative">
+      <div className="flex justify-center">
+        <div className="relative sm:w-[1350px] w-[340px] h-[470px]">
+          <Image
+            src={images[currImageIndex].src}
+            alt={images[currImageIndex].alt}
+            fill
+            className="rounded-xl sm:object-cover sm:object-center"
+          />
+          <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black/80 via-black/30 to-transparent pointer-events-none"></div>
+
+          <div className="absolute bottom-8 left-8 z-10">
+            <p className="text-white mb-1">{images[currImageIndex].year}</p>
+            <p className="font-bold text-md sm:text-4xl text-white mb-3">
+              {images[currImageIndex].name}
+            </p>
+            <Link
+              href={images[currImageIndex].link}
+              className="inline-block hover:bg-gray-300 active:scale-105 transition-all duration-300 font-semibold px-3 sm:px-4 rounded-md py-2 bg-white text-black"
+            >
               Go To Show
-            </button>
-          </Link>
+            </Link>
+          </div>
+
+          <Link
+            href={images[currImageIndex].link}
+            className="absolute inset-0 rounded-xl"
+            aria-label={`View ${images[currImageIndex].name}`}
+          />
         </div>
       </div>
     </div>
