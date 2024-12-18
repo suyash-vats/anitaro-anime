8d8a12d00cdf88eb34737d42bf4bfe2ab4292af0
akshitvigg <akshitvig48@gmail.com>
2024-12-18 22:12:52 +0530

    s

--- config.ts
+++ config.ts
@@ -1,4 +1,3 @@
 export const CONSUMET_URL =
   "https://consumet-api-9p6q.onrender.com/anime/gogoanime";
-// export const MANGA_URL = "https://consumet-api-9p6q.onrender.com/manga";
-export const MANGA_URL = "https://cors-proxy-fad0.onrender.com/proxy/manga";
+export const MANGA_URL = "https://consumet-api-9p6q.onrender.com/manga";
