6eeb7eb34661c3175afc21b08ed0ff1e49df915d
akshitvigg <akshitvig48@gmail.com>
2024-12-18 17:18:33 +0530

    update

--- app/manga/page.tsx
+++ app/manga/page.tsx
@@ -13,7 +13,7 @@ const MangaHomePage = () => {
   const [mangaId, setMangaId] = useState<string>("");
 
   const getManga = async (path: string) => {
-    const response: any = await axios.get(`${MANGA_URL}/mangadex/popular`);
+    const response: any = await axios.get(`${MANGA_URL}/mangakakalot/${path}`);
     setMangadata(response.data.results);
     setActiveBtn(path);
   };
