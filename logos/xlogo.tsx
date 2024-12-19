cb1adeb388979dfc326868110920a8e6b6edcecd
akshitvigg <akshitvig48@gmail.com>
2024-12-19 19:55:34 +0530

    xlogo

---
+++ logos/xlogo.tsx
@@ -0,0 +1,35 @@
+export const Xlogo = () => {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="30"
+        height="30"
+        viewBox="0,0,300,150"
+      >
+        <g
+          fill="#ffffff"
+          fillRule="nonzero"
+          stroke="none"
+          strokeWidth="1"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
+          className="  mix-blend-mode: normal h-6 w-6 hover:scale-110 transition-transform"
+        >
+          <g transform="scale(5.12,5.12)">
+            <path d="M6.91992,6l14.2168,20.72656l-14.9082,17.27344h3.17773l13.13867,-15.22266l10.44141,15.22266h10.01367l-14.87695,-21.6875l14.08008,-16.3125h-3.17578l-12.31055,14.26172l-9.7832,-14.26172z"></path>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+};
