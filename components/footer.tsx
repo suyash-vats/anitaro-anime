d83092b90f3c2d3f68f1a595191d73d8c9db27f6
akshitvigg <akshitvig48@gmail.com>
2024-12-19 21:57:01 +0530

    footer updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,44 +6,51 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <Navbar isBordered className="border-t mt-10 font-mono py-4">
-      <NavbarBrand className="flex justify-between items-center ">
-        <div className="flex items-center space-x-4">
-          <Copy />
-          <Link
-            href="/"
-            className="font-bold font-mono text-xl bg-gradient-to-r text-white bg-clip-text text-transparent hover:scale-105 transition-transform"
-          >
-            アニタロ
-          </Link>
-        </div>
+    <Navbar
+      isBordered
+      className="border-t mt-10 font-mono py-6 flex flex-col md:flex-row items-center md:justify-between"
+    >
+      <div className="flex items-center space-x-4 mb-4 md:mb-0">
+        <Copy />
+        <Link
+          href="/"
+          className="font-bold font-mono text-xl bg-gradient-to-r text-white bg-clip-text text-transparent hover:scale-105 transition-transform"
+        >
+          アニタロ
+        </Link>
+      </div>
 
-        <div className="text-white text-sm text-center mt-2 md:mt-0">
-          <p>
-            Developed by
-            <span className="pl-2">
-              <a
-                target="_blank"
-                href="https://github.com/akshitvigg"
-                className="hover:underline"
-              >
-                Akshit
-              </a>
-            </span>
-          </p>
-        </div>
-
-        <div className="flex items-center space-x-4 text-white">
-          <a target="_blank" href="https://github.com/akshitvigg/anitaro">
-            <Gitlogo />
-          </a>
-          <div className=" -translate-y-1">
-            <a target="_blank" href="https://x.com/AkshitVig4">
-              <Xlogo />
+      <div className="text-white text-sm text-center mb-4 md:mb-0">
+        <p>
+          Developed by
+          <span className="pl-2">
+            <a
+              target="_blank"
+              href="https://github.com/akshitvigg"
+              className="hover:underline"
+            >
+              Akshit
             </a>
-          </div>
-        </div>
-      </NavbarBrand>
+          </span>
+        </p>
+      </div>
+
+      <div className="flex items-center space-x-4 text-white">
+        <a
+          target="_blank"
+          href="https://github.com/akshitvigg/anitaro"
+          className="hover:scale-110 transition-transform"
+        >
+          <Gitlogo />
+        </a>
+        <a
+          target="_blank"
+          href="https://x.com/AkshitVig4"
+          className="hover:scale-110 transition-transform"
+        >
+          <Xlogo />
+        </a>
+      </div>
     </Navbar>
   );
 };
