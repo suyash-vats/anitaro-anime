09357d4eba6ca15526fc74297c14fcbf3f1b886d
Akshit Vig <akshitvig48@gmail.com>
2024-12-20 16:00:39 +0530

    added spinner

--- app/searchManga/[mangaid]/page.tsx
+++ app/searchManga/[mangaid]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { MangaNavbar } from "@/components/mangaNavbar";
 import { MANGA_URL } from "@/config";
-import { Image } from "@nextui-org/react";
+import { Image, Spinner } from "@nextui-org/react";
 import axios from "axios";
 
 import Link from "next/link";
@@ -10,6 +10,7 @@ import { useEffect, useState, use } from "react";
 const GetsearchManga = ({ params }: any) => {
   const searchquery: any = use(params);
   const mangaid = searchquery.mangaid;
+  const [loading, setLoading] = useState<boolean>(true);
 
   const [mangaData, setmangadata] = useState<any[]>([]);
   const getMangadata = async () => {
@@ -17,6 +18,7 @@ const GetsearchManga = ({ params }: any) => {
       `${MANGA_URL}/mangadex/${mangaid}`
     );
     setmangadata(response.data.results);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -28,32 +30,38 @@ const GetsearchManga = ({ params }: any) => {
   return (
     <div>
       <MangaNavbar />
-      <div className=" flex justify-center">
-        <div className=" sm:ml-[80px] ml-4 mr-4 sm:mr-[80px]  bg-black mt-32 mb-16  sm:mt-12  grid gap-x-5 gap-y-4 sm:grid-cols-3">
-          {mangaData.map((manga) => (
-            <div className="   col-span-1 " key={manga.id}>
-              <Link href={`/mangainfo/${manga.id}`}>
-                <div>
-                  <Image
-                    // isBlurred={true}
-                    isZoomed
-                    className=" mb-2 border border-[#3f3f46] object-cover"
-                    loading="lazy"
-                    width={400}
-                    height={240}
-                    radius="sm"
-                    src={getProxyImageUrl(manga.image)}
-                  />
-
-                  {manga.title.length > 60
-                    ? manga.title.slice(0, 17) + "..."
-                    : manga.title}
-                </div>
-              </Link>
-            </div>
-          ))}
+      {loading ? (
+        <div className=" flex justify-center mb-80 mt-52">
+          <Spinner size="lg" />
         </div>
-      </div>
+      ) : (
+        <div className=" flex justify-center">
+          <div className=" sm:ml-[80px] ml-4 mr-4 sm:mr-[80px]  bg-black mt-32 mb-16  sm:mt-12  grid gap-x-5 gap-y-4 sm:grid-cols-3">
+            {mangaData.map((manga) => (
+              <div className="   col-span-1 " key={manga.id}>
+                <Link href={`/mangainfo/${manga.id}`}>
+                  <div>
+                    <Image
+                      // isBlurred={true}
+                      isZoomed
+                      className=" mb-2 border border-[#3f3f46] object-cover"
+                      loading="lazy"
+                      width={400}
+                      height={240}
+                      radius="sm"
+                      src={getProxyImageUrl(manga.image)}
+                    />
+
+                    {manga.title.length > 60
+                      ? manga.title.slice(0, 17) + "..."
+                      : manga.title}
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
